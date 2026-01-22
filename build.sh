a9683ec5d0d7d39609c47208707314c8f09b9cc8
likakuli <1154584512@qq.com>
2026-01-22 22:22:16 +0800

    rebuilding site 2026年 1月22日 星期四 22时22分15秒 CST

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Generate algolia index
-hugo-algolia -s
+hugo-algolia -s || echo "Warning: Algolia index generation failed, continuing..."
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
